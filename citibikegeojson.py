897d443f8483c59f53203cfb23481c97747319fb
zingbot <jferzoco@gmail.com>
2016-10-02 17:19:56 -0400

    server update

--- citibikegeojson.py
+++ citibikegeojson.py
@@ -1,25 +1,49 @@
-from sys import argv
+import urllib, json
 from os.path import exists
-import simplejson as json 
- 
-script, in_file, out_file = argv
- 
-data = json.load(open(in_file))
- 
-geojson = {
-    "type": "FeatureCollection",
-    "features": [
-    {
-        "type": "Feature",
-        "geometry" : {
-            "type": "Point",
-            "coordinates": [d["lon"], d["lat"]],
-            },
-        "properties" : d,
-     } for d in data]
-}
+import json 
+from flask import Flask
 
-output = open(out_file, 'w')
-json.dump(geojson, output)
- 
-print geojson
+app = Flask(__name__)
+
+
+@app.route('/')
+def process():
+    url = "https://gbfs.citibikenyc.com/gbfs/en/station_information.json"
+    response = urllib.urlopen(url)
+    print response
+    data = json.loads(response.read())
+     
+    # data = json.load(open(in_file))
+    result  = []
+    for station in data['data']['stations']: 
+        print station
+        if 'region_id' in station:
+            region_id = station['region_id']
+        else:
+            region_id = None 
+
+        result.append({
+            'geometry':{
+                'type': 'Point',
+                "coordinates" : [station['lon'], station['lat']]
+            },
+            'type' : "Feature",
+            'properties' : {
+                'station_id' : station['station_id'],
+                'name' : station['name'],
+                'region_id' : region_id,
+                'capacity' : station['capacity'],
+                'eightd_has_key_dispenser' : station['eightd_has_key_dispenser'],
+                'KEY' : 'KEY' in station['rental_methods'],
+                'CREDITCARD' : 'CREDITCARD' in station['rental_methods']
+            }
+        })
+
+    geojson = {
+        "type": "FeatureCollection",
+        "features": result
+    }
+
+    # output = open('city_bike.geojson', 'w')
+    return json.dumps(geojson)
+     
